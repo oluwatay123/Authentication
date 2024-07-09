31f3194fba201a6038e319dc8cb8bb33b9095802
cyberspaceX <oluwatayodavid273@gmail,com>
2024-07-09 11:21:12 +0100

    update

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./src/app');
-
-const PORT = process.env.PORT || 6000;
-
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-module.exports = server;
